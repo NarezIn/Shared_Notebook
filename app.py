8e34045cc9bf263528e2fa3a6cbdc20cd55bcf6a
NarezIn <zn2133@nyu.edu>
2026-01-12 12:48:04 -0500

    add a userColumn, but now the notebook is append-only

--- app.py
+++ app.py
@@ -1,7 +1,28 @@
 from flask import Flask, request, jsonify, render_template
+import json
 
 app = Flask(__name__)
 
+# ---------- Helpers ----------
+def load_users():
+    #Should I import os and do if os.path exists the json file?
+    with open("users.json", "r", encoding = "utf-8") as f:
+        return json.load(f)
+    
+def save_users(users):
+    with open("users.json", "w", encoding = "utf-8") as f:
+        json.dump(users, f)#dumps new all users to the json file.
+
+def load_notes():
+    """Read in the json file and convert to a python dictionary."""
+    with open("notes_data.json", "r", encoding = "utf-8") as f:
+        return json.load(f)
+    
+def save_notes(notes):
+    with open("notes_data.json", "w", encoding="utf-8") as f:
+        json.dump(notes, f)
+
+# ---------- Routes -----------
 @app.route("/")
 def index():
     """
@@ -10,20 +31,32 @@ def index():
     """
     return render_template("index.html")
 
+@app.route("/new_anony_user")
+def new_anony_user():
+    """If frontend cannot find the user in localStorage, needs to create a new anony username"""
+    all_users = load_users()
+    new_anony_userid = len(all_users) + 1
+    new_anony_username = f'Anony{new_anony_userid:03d}'
+    all_users.append({"username": new_anony_username})
+    save_users(all_users)
+    return jsonify({"username": new_anony_username})
+
 @app.route("/note", methods = ["GET"])
 def read_note():
-    #this is like the GET request, the browser 'get' data from backend to display
-    with open("notes_data.txt", "r", encoding = "utf-8") as f:
-        content = f.read()
-    return content
+    """this is like the GET request (well, exactly the GET request), the browser 'get' data from backend to display"""
+    notes = load_notes()
+    return jsonify(notes)
 
 @app.route("/note", methods = ["POST"])
 def write_note():
     data = request.json # chatgpt: {"text": "xxxx"} from frontend
-    text = data.get("text", "Default value, meaning errorrrrrr") # this would be the 'text' written by the user in frontend
-    with open("notes_data.txt", "w", encoding = "utf-8") as f:
-        f.write(text)
-    return jsonify({"status": "ok"}) # try not jsonify
+    username = data.get("username", "Anony000")
+    text = data.get("text", "placeholder, meaning errorrrrrr!") # this would be the 'text' written by the user in frontend
+
+    notes = load_notes()
+    notes.append({"username": username, "text": text})
+    save_notes(notes)
+    return jsonify({"status": "ok"}) # try not jsonify?
 
 if __name__ == "__main__":
     app.run(debug = True)
